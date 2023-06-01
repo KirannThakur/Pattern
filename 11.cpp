d2e746cb2abe53641df7bc5e4ff5625dd4416a8d
KirannThakur <134675862+KirannThakur@users.noreply.github.com>
2023-06-01 18:12:42 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int row, col, n;
+  cout<<"enter n\n";
+  cin>>n;
+  for(row=0; row<n; row++)
+  {
+   for(col=0; col<n-row; col++)
+   {
+     cout<<" ";
+   }
+   for(col=0; col<row+1; col++)
+   {
+     cout<<col+1;
+   }
+   int a=row;
+   for(col=0; col<row; col++)
+   {
+     cout<<a;
+     a=a-1;
+   }
+  cout<<endl;
+  }
+}
+
+
+  
