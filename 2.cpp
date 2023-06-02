dd69c20973f2f1cc6612e96d390194f5e6ccc459
KirannThakur <134675862+KirannThakur@users.noreply.github.com>
2023-06-02 09:31:23 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 int main()
 {
-  int row, col, n;
-  cout<<"enter n \n";
-  cin>>n;
-  for(row=0; row<n; row++)
-  {
-    for(col=0; col<n-row; col++)
-    {
-      cout<<" ";
-    }
-    for(col=0; col<row+1; col++)
-    {
-      cout<<"*";
-    }
-    for(col=0; col<row; col++)
-    {
-      cout<<"*";
-    }
-    cout<<endl;
-  }
+	int n, row, col;
+	cout<<"enter n";
+	cin>>n;
+	for(row=0; row<n; row++)
+	{
+		int k=0;
+		for(col=0; col<((2*n)-1); col++)
+      {
+      	if(col<n-row-1)
+      	{
+		cout<<" ";
+      }
+      else if(k<(2*row)+1)
+      {	cout<<"*";
+		  k++;
+	  }
+	  else
+	  {
+	  	cout<<" ";
+	  }
+	  }
+	  cout<<endl;
+	}
 }
